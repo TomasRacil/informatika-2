fc15bdcec6f73e7fb23535bd13aef80b47498dae
tomasracil <tomas.racil@unob.cz>
2026-03-24 12:44:31 +0100

    aoc template

--- priklady-z-hodin/2025-2026LS/aoc_20_1/main.cpp
+++ priklady-z-hodin/2025-2026LS/aoc_20_1/main.cpp
@@ -1,3 +1,26 @@
+#include <string>  
+#include <fstream>  
 #include <iostream>
+#include <vector>
 
-int main() { return 0; }
+using namespace std;
+
+int main(){
+
+    ifstream soubor("data.txt");  
+    string radek;
+    vector<int> cisla;
+
+    if (soubor.is_open()) {  
+        while (getline(soubor, radek)) {
+            cisla.push_back(stoi(radek));
+        }  
+        soubor.close();  
+    }
+
+    for(int i = 0; i<cisla.size();i++){
+        std::cout<< cisla[i]<<std::endl;
+    }
+
+    return 0;
+}
