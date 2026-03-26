3be2723d16dfedfea6af4d8d5d928bd12ea6dee7
tomasracil <tomas.racil@unob.cz>
2026-03-26 09:29:55 +0100

    maticova kalkulacka v1

---
+++ priklady-z-hodin/2025-2026LS/maticova_kalkulacka/main.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+
+void vypis_matici(int matice[][3], int radky){
+
+    std::cout<<"Matice:"<<std::endl;
+    for(int i = 0; i<radky; i++){
+        for(int j=0;j<3;j++){
+            std::cout<<matice[i][j]<<" ";;
+        }
+        std::cout<<std::endl;
+    }
+
+}
+
+void napln_matici(int matice[][3], int radky){
+
+    std::cout<<"Napln matici:"<<std::endl;
+    for(int i = 0; i<radky; i++){
+        for(int j=0;j<3;j++){
+            std::cin>>matice[i][j];
+        }
+    }
+
+}
+
+void secti_matice(int matice_1[][3],int matice_2[][3], int matice_3[][3] , int radky){
+    std::cout<<"Soucet matic:"<<std::endl;
+    for(int i = 0; i<radky; i++){
+        for(int j=0;j<3;j++){
+            matice_3[i][j]=matice_1[i][j]+matice_2[i][j];
+        }
+    }
+}
+
+int main(){
+
+    int matice_1[3][3]={{1,2,3},{4,5,6},{7,8,9}};
+    int matice_2[3][3];
+    int matice_3[3][3];
+
+    vypis_matici(matice_1, 3);
+
+    napln_matici(matice_2, 3);
+
+    vypis_matici(matice_2, 3);
+
+    secti_matice(matice_1,matice_2, matice_3, 3);
+
+    vypis_matici(matice_3, 3);
+
+    return 0;
+}
