e5fa54ff9a6647067e996a527af3565fae8014d7
tomasracil <tomas.racil@unob.cz>
2026-04-29 10:19:36 +0200

    priklady z hodin reference a pointery

--- priklady-z-hodin/2025-2026LS/linked_list/main.cpp
+++ priklady-z-hodin/2025-2026LS/linked_list/main.cpp
@@ -2,57 +2,71 @@
 
 // Struktura jednoho uzlu seznamu
 struct Node {
-    int data;
-    Node* next;
+  int data;
+  Node *next;
 };
 
 // ÚKOL 1: Přidání prvku na ZAČÁTEK seznamu
-// Všimněte si 'Node*& head' - je to reference na pointer! Můžeme tak změnit samotný head.
-void insertFront(Node*& head, int value) {
-    // 1. Vytvořte nový uzel pomocí operátoru 'new'
-    // 2. Naplňte ho daty
-    // 3. Jeho 'next' musí ukazovat na současný první prvek (head)
-    // 4. Přesuňte 'head', aby ukazoval na tento nový uzel
-    
-    // Váš kód zde:
-    
+// Všimněte si 'Node*& head' - je to reference na pointer! Můžeme tak změnit
+// samotný head.
+void insertFront(Node *&head, int value) {
+  // 1. Vytvořte nový uzel pomocí operátoru 'new'
+  // 2. Naplňte ho daty
+  // 3. Jeho 'next' musí ukazovat na současný první prvek (head)
+  // 4. Přesuňte 'head', aby ukazoval na tento nový uzel
+
+  // Váš kód zde:
+  Node *newNode = new Node;
+  newNode->data = value;
+  // (*newNode).data =value;
+  newNode->next = head;
+  head = newNode;
 }
 
 // ÚKOL 2: Výpis celého seznamu
-void printList(Node* head) {
-    // Projděte seznam od 'head' až do konce (dokud nenarazíte na nullptr)
-    // a vypisujte hodnoty.
-    
-    // Váš kód zde:
-    
-    std::cout << "nullptr\n";
+void printList(Node *head) {
+  // Projděte seznam od 'head' až do konce (dokud nenarazíte na nullptr)
+  // a vypisujte hodnoty.
+
+  // Váš kód zde:
+  while (head != nullptr) {
+    std::cout << "Adress: " << head << " Data: " << head->data
+              << " Next: " << head->next << " ->\n";
+    head = head->next;
+  }
+  std::cout << "nullptr\n";
 }
 
 // ÚKOL 3: Uvolnění paměti (prevence memory leaku)
-void deleteList(Node*& head) {
-    // Záludnost: Nemůžete smazat uzel a PAK se ptát na jeho 'next'.
-    // Musíte si 'next' nejdříve uložit bokem!
-    // K mazání použijte operátor 'delete'.
-    
-    // Váš kód zde:
-    
-    head = nullptr; // Nakonec nastavíme head bezpečně na nic
+void deleteList(Node *&head) {
+  // Záludnost: Nemůžete smazat uzel a PAK se ptát na jeho 'next'.
+  // Musíte si 'next' nejdříve uložit bokem!
+  // K mazání použijte operátor 'delete'.
+
+  // Váš kód zde:
+
+  // head = nullptr; // Nakonec nastavíme head bezpečně na nic
 }
 
 int main() {
-    Node* head = nullptr; // Na začátku je seznam prázdný
+  Node *head = nullptr; // Na začátku je seznam prázdný
 
-    insertFront(head, 10);
-    insertFront(head, 20);
-    insertFront(head, 30);
+  std::cout << "Vypis seznamu: \n";
+  printList(head);
+  insertFront(head, 10);
+  std::cout << "Vypis seznamu (10): \n";
+  printList(head);
+  insertFront(head, 20);
+  std::cout << "Vypis seznamu (20): \n";
+  printList(head);
+  insertFront(head, 30);
+  std::cout << "Vypis seznamu (30): \n";
+  printList(head); // Očekávaný výstup: 30 -> 20 -> 10 -> nullptr
 
-    std::cout << "Vypis seznamu: ";
-    printList(head); // Očekávaný výstup: 30 -> 20 -> 10 -> nullptr
+  deleteList(head);
 
-    deleteList(head);
-    
-    std::cout << "Po smazani: ";
-    printList(head); // Očekávaný výstup: nullptr
+  std::cout << "Po smazani: \n";
+  printList(head); // Očekávaný výstup: nullptr
 
-    return 0;
+  return 0;
 }
--- priklady-z-hodin/2025-2026LS/swap_hodnot/main.cpp
+++ priklady-z-hodin/2025-2026LS/swap_hodnot/main.cpp
@@ -1,28 +1,33 @@
 #include <iostream>
 
 // ÚKOL 1: Doplňte prohození hodnot pomocí pointrů
-void swapPointer(int* a, int* b) {
-    // Váš kód zde
-    
+void swapPointer(int *a, int *b) {
+  // int temp = *a;
+  // *a = *b;
+  // *b = temp;
+  *a = *a + *b;
+  *b = *a - *b;
+  *a = *a - *b;
 }
 
 // ÚKOL 2: Doplňte prohození hodnot pomocí referencí
-void swapReference(int& a, int& b) {
-    // Váš kód zde
-    
+void swapReference(int &a, int &b) {
+  int temp = a;
+  a = b;
+  b = temp;
 }
 
 int main() {
-    int x = 10, y = 20;
-    std::cout << "Puvodni: x=" << x << ", y=" << y << "\n";
+  int x = 10, y = 20;
+  std::cout << "Puvodni: x=" << x << ", y=" << y << "\n";
 
-    // Volání pointer verze (nezapomeňte poslat adresy!)
-    swapPointer(&x, &y);
-    std::cout << "Po swapPointer: x=" << x << ", y=" << y << "\n";
+  // Volání pointer verze (nezapomeňte poslat adresy!)
+  swapPointer(&x, &y);
+  std::cout << "Po swapPointer: x=" << x << ", y=" << y << "\n";
 
-    // Volání referenční verze
-    swapReference(x, y);
-    std::cout << "Po swapReference: x=" << x << ", y=" << y << "\n";
+  // Volání referenční verze
+  swapReference(x, y);
+  std::cout << "Po swapReference: x=" << x << ", y=" << y << "\n";
 
-    return 0;
+  return 0;
 }
