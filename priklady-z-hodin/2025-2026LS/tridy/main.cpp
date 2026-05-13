160e1a216183aeae4d564d3743a12b0701622684
tomasracil <tomas.racil@unob.cz>
2026-05-13 12:44:06 +0200

    tridy konstruktor destruktor

--- priklady-z-hodin/2025-2026LS/tridy/main.cpp
+++ priklady-z-hodin/2025-2026LS/tridy/main.cpp
@@ -5,50 +5,83 @@ class Auto
 private:
     std::string znacka;
     int rokVyroby;
+
 public:
-  Auto();
-  Auto(std::string znacka, int rokVyroby);
-  void nastavHodnoty(std::string znacka, int rokVyroby);
-  void vypisInfo(){
-    std::cout<<"Vozidlo se znackou: "<<znacka<<" a rokem vyroby: " <<rokVyroby<<std::endl;
-  }
-  ~Auto();
+    Auto();
+    Auto(std::string znacka, int rokVyroby);
+    void nastavHodnoty(std::string znacka, int rokVyroby);
+    void vypisInfo()
+    {
+        std::cout << "Vozidlo se znackou: " << znacka << " a rokem vyroby: " << rokVyroby << std::endl;
+    }
+    ~Auto();
 };
 
-int main(){
+class Produkt
+{
+private:
+    std::string nazev;
+    double cena;
 
-    Auto auto1("Skoda",2010);
+public:
+    Produkt(std::string nazev, double cena);
+    ~Produkt();
+};
+
+int main()
+{
+
+    Produkt produkt1("Telefon", 5999.99);
     {
-        Auto auto2("Fiat",2005);
+        Produkt produkt2("Televize", 15999.99);
     }
+    Produkt *produkt3 = new Produkt("PC", 10999.99);
 
-    auto1.vypisInfo();
-    auto1.nastavHodnoty("Ford", 2015);
-    auto1.vypisInfo();
+    delete produkt3;
 
+    // Auto auto1("Skoda", 2010);
+    // {
+    //     Auto auto2("Fiat", 2005);
+    // }
+
+    // auto1.vypisInfo();
+    // auto1.nastavHodnoty("Ford", 2015);
+    // auto1.vypisInfo();
 
     return 0;
 }
 
 Auto::Auto()
 {
-    this->znacka="";
-    this->rokVyroby=0;
+    this->znacka = "";
+    this->rokVyroby = 0;
 }
 
 Auto::Auto(std::string znacka, int rokVyroby)
 {
-    this->znacka=znacka;
+    this->znacka = znacka;
     this->rokVyroby = rokVyroby;
 }
 
 void Auto::nastavHodnoty(std::string znacka, int rokVyroby)
 {
-    this->znacka=znacka;
+    this->znacka = znacka;
     this->rokVyroby = rokVyroby;
 }
 
 Auto::~Auto()
 {
-    std::cout<<znacka<<" s rokem vyroby "<<rokVyroby<<" smazana"<<std::endl;
+    std::cout << znacka << " s rokem vyroby " << rokVyroby << " smazana" << std::endl;
+}
+
+Produkt::Produkt(std::string nazev, double cena)
+{
+    std::cout << "Vytvořen produkt: " << nazev << std::endl;
+    this->nazev = nazev;
+    this->cena = cena;
+}
+
+Produkt::~Produkt()
+{
+    std::cout << "Odstranen produkt: " << nazev << std::endl;
 }
