962662cfdb2ad134d80cdc0e94c91c4ba3283b47
tomasracil <tomas.racil@unob.cz>
2026-05-20 12:23:18 +0200

    Maticova kalkulacka tridy

--- priklady-z-hodin/2025-2026LS/maticova_kalkulacka/main.cpp
+++ priklady-z-hodin/2025-2026LS/maticova_kalkulacka/main.cpp
@@ -1,21 +1,29 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <stdexcept>
 
-struct Matice {
+class Matice
+{
+private:
   int **matice;
   int radky;
   int sloupce;
+
+public:
+  Matice(int radky, int sloupce, int inicializacni_hodnota = NULL);
+  Matice(const Matice &other);
+  void vypis_matici();
+  Matice secti_matice(const Matice &matice_2);
+  Matice nasob_matice(const Matice &matice_2);
+  Matice transponuj_matici();
+  Matice skalarni_nasobeni(int scalar);
+  int determinant_matice();
+  ~Matice();
 };
 
-Matice *vytvor_matici(int radky, int sloupce, int inicializacni_hodnota = NULL);
-void smaz_matici(Matice *&matice);
-void vypis_matici(Matice *matice);
-Matice *secti_matice(const Matice &matice_1, const Matice &matice_2);
-Matice *nasob_matice(const Matice &matice_1, const Matice &matice_2);
-Matice *transponuj_matici(const Matice &matice);
-Matice *skalarni_nasobeni(const Matice &matice, int scalar);
-int determinant_matice(const Matice &matice);
-
-int main() {
+int main()
+{
   srand(time(0));
 
   std::cout << "Zadej pocet radku prvni matice: ";
@@ -26,7 +34,7 @@ int main() {
   int sloupceA;
   std::cin >> sloupceA;
 
-  Matice *maticeA = vytvor_matici(radkyA, sloupceA);
+  Matice maticeA = Matice(radkyA, sloupceA);
 
   std::cout << "Zadej pocet radku druhe matice: ";
   int radkyB;
@@ -36,150 +44,166 @@ int main() {
   int sloupceB;
   std::cin >> sloupceB;
 
-  Matice *maticeB = vytvor_matici(radkyB, sloupceB);
+  Matice maticeB = Matice(radkyB, sloupceB);
 
-  vypis_matici(maticeA);
-  vypis_matici(maticeB);
+  maticeA.vypis_matici();
+  maticeB.vypis_matici();
 
   std::cout << "Soucet matic A a B:" << std::endl;
-  Matice *soucet = secti_matice(*maticeA, *maticeB);
-  vypis_matici(soucet);
+  Matice soucet = maticeA.secti_matice(maticeB);
+  soucet.vypis_matici();
 
   std::cout << "Soucin matic A a B:" << std::endl;
-  Matice *nasobek = nasob_matice(*maticeA, *maticeB);
-  vypis_matici(nasobek);
+  Matice soucin = maticeA.nasob_matice(maticeB);
+  soucin.vypis_matici();
 
   std::cout << "Transponovana maticeA:" << std::endl;
-  Matice *transponovany_maticeA = transponuj_matici(*maticeA);
-  vypis_matici(transponovany_maticeA);
+  Matice transponovany_maticeA = maticeA.transponuj_matici();
+  transponovany_maticeA.vypis_matici();
 
   std::cout << "Skalarni nasobeni maticeA cislem 2:" << std::endl;
-  Matice *skalarni = skalarni_nasobeni(*maticeA, 2);
-  vypis_matici(skalarni);
+  Matice skalarni = maticeA.skalarni_nasobeni(2);
+  skalarni.vypis_matici();
 
-  int determinant_maticeA = determinant_matice(*maticeA);
-  std::cout << "Determinant matice A: " << determinant_maticeA << std::endl;
-  int determinant_maticeB = determinant_matice(*maticeB);
-  std::cout << "Determinant matice B: " << determinant_maticeB << std::endl;
-
-  smaz_matici(maticeA);
-  smaz_matici(maticeB);
-  smaz_matici(soucet);
-  smaz_matici(nasobek);
-  smaz_matici(transponovany_maticeA);
-  smaz_matici(skalarni);
+  std::cout << "Determinant maticeA: " << maticeA.determinant_matice() << std::endl;
+  std::cout << "Determinant maticeB: " << maticeB.determinant_matice() << std::endl;
 
   return 0;
 }
 
-Matice *vytvor_matici(int radky, int sloupce, int inicializacni_hodnota) {
-  Matice *nova_matice = new Matice;
-  nova_matice->matice = nullptr;
-  nova_matice->radky = radky;
-  nova_matice->sloupce = sloupce;
 
-  nova_matice->matice = new int *[nova_matice->radky];
+Matice::Matice(int radky, int sloupce, int inicializacni_hodnota)
+{
+  this->radky = radky;
+  this->sloupce = sloupce;
 
-  for (int i = 0; i < nova_matice->radky; ++i) {
-    nova_matice->matice[i] = new int[nova_matice->sloupce];
-    for (int j = 0; j < nova_matice->sloupce; j++) {
-      if (inicializacni_hodnota == NULL) {
-        nova_matice->matice[i][j] = rand() % 10;
-      } else {
-        nova_matice->matice[i][j] = inicializacni_hodnota;
+  matice = new int *[radky];
+  for (int i = 0; i < radky; ++i)
+  {
+    matice[i] = new int[sloupce];
+    for (int j = 0; j < sloupce; j++)
+    {
+      if (inicializacni_hodnota == NULL)
+      {
+        matice[i][j] = rand() % 10;
+      }
+      else
+      {
+        matice[i][j] = inicializacni_hodnota;
       }
     }
   }
-  return nova_matice;
 }
 
-void smaz_matici(Matice *&matice) {
-  if (matice == nullptr) {
-    return;
+Matice::Matice(const Matice &other)
+{
+  this->radky = other.radky;
+  this->sloupce = other.sloupce;
+  this->matice = new int *[radky];
+  for (int i = 0; i < radky; i++)
+  {
+    this->matice[i] = new int[sloupce];
+    for (int j = 0; j < sloupce; j++)
+    {
+      this->matice[i][j] = other.matice[i][j];
+    }
   }
-  for (int i = 0; i < matice->radky; ++i) {
-    delete[] matice->matice[i];
-    matice->matice[i] = nullptr;
-  }
-  delete[] matice->matice;
-  matice = nullptr;
-
-  delete matice;
-  matice = nullptr;
 }
 
-void vypis_matici(Matice *matice) {
+void Matice::vypis_matici()
+{
   std::cout << "Matice:" << std::endl;
-  if (matice == nullptr) {
+  if (matice == nullptr)
+  {
     std::cout << "Matice je prazdna." << std::endl;
     return;
   }
-  for (int i = 0; i < matice->radky; i++) {
-    for (int j = 0; j < matice->sloupce; j++) {
-      std::cout << matice->matice[i][j] << " ";
+  for (int i = 0; i < radky; i++)
+  {
+    for (int j = 0; j < sloupce; j++)
+    {
+      std::cout << matice[i][j] << " ";
     }
     std::cout << std::endl;
   }
 }
 
-Matice *secti_matice(const Matice &matice_1, const Matice &matice_2) {
-  if (matice_1.radky != matice_2.radky ||
-      matice_1.sloupce != matice_2.sloupce) {
-    std::cout << "Matice nejsou stejne velke." << std::endl;
-    return nullptr;
+Matice Matice::secti_matice(const Matice &matice_2)
+{
+  if (radky != matice_2.radky || sloupce != matice_2.sloupce)
+  {
+    throw std::runtime_error("Matice nejsou stejne velke.");
   }
-  Matice *vysledek = vytvor_matici(matice_1.radky, matice_1.sloupce, 0);
-  for (int i = 0; i < matice_1.radky; i++) {
-    for (int j = 0; j < matice_1.sloupce; j++) {
-      vysledek->matice[i][j] = matice_1.matice[i][j] + matice_2.matice[i][j];
+  Matice vysledek(radky, sloupce);
+  for (int i = 0; i < radky; i++)
+  {
+    for (int j = 0; j < sloupce; j++)
+    {
+      vysledek.matice[i][j] = matice[i][j] + matice_2.matice[i][j];
     }
   }
   return vysledek;
 }
 
-Matice *nasob_matice(const Matice &matice_1, const Matice &matice_2) {
-  if (matice_1.radky != matice_2.sloupce) {
-    std::cout << "Matice nejsou kompatibilni pro nasobeni." << std::endl;
-    return nullptr;
+Matice Matice::nasob_matice(const Matice &matice_2)
+{
+  if (sloupce != matice_2.radky) {
+    throw std::runtime_error("Matice nejsou kompatibilni pro nasobeni.");
   }
-  Matice *vysledek = vytvor_matici(matice_1.radky, matice_2.sloupce, 0);
-  for (int i = 0; i < matice_1.radky; i++) {
+  Matice vysledek = Matice(radky, matice_2.sloupce, 0);
+  for (int i = 0; i < radky; i++) {
     for (int j = 0; j < matice_2.sloupce; j++) {
-      for (int k = 0; k < matice_1.sloupce; k++) {
-        vysledek->matice[i][j] +=
-            (matice_1.matice[i][k] * matice_2.matice[k][j]);
+      for (int k = 0; k < sloupce; k++) {
+        vysledek.matice[i][j] +=
+            (matice[i][k] * matice_2.matice[k][j]);
       }
     }
   }
   return vysledek;
 }
 
-Matice *transponuj_matici(const Matice &matice) {
-  Matice *vysledek = vytvor_matici(matice.sloupce, matice.radky, 0);
-  for (int i = 0; i < matice.sloupce; i++) {
-    for (int j = 0; j < matice.radky; j++) {
-      vysledek->matice[i][j] = matice.matice[j][i];
+Matice Matice::transponuj_matici()
+{
+    Matice vysledek = Matice(sloupce, radky, 0);
+  for (int i = 0; i < sloupce; i++) {
+    for (int j = 0; j < radky; j++) {
+      vysledek.matice[i][j] = matice[j][i];
     }
   }
   return vysledek;
 }
 
-Matice *skalarni_nasobeni(const Matice &matice, int scalar) {
-  Matice *vysledek = vytvor_matici(matice.radky, matice.sloupce, 0);
-  for (int i = 0; i < matice.radky; i++) {
-    for (int j = 0; j < matice.sloupce; j++) {
-      vysledek->matice[i][j] = matice.matice[i][j] * scalar;
+Matice Matice::skalarni_nasobeni(int scalar)
+{
+  Matice vysledek = Matice(radky, sloupce, 0);
+  for (int i = 0; i < radky; i++) {
+    for (int j = 0; j < sloupce; j++) {
+      vysledek.matice[i][j] = matice[i][j] * scalar;
     }
   }
   return vysledek;
 }
 
-int determinant_matice(const Matice &matice) {
+int Matice::determinant_matice()
+{
   int vysledek = 0;
-  for (int i = 0; i < matice.radky; i++) {
-    for (int j = 0; j < matice.sloupce; j++) {
-      vysledek += matice.matice[i][j];
+  for (int i = 0; i < radky; i++)
+  {
+    for (int j = 0; j < sloupce; j++)
+    {
+      vysledek += matice[i][j];
     }
   }
   return vysledek;
+}
+
+Matice::~Matice()
+{
+  for (int i = 0; i < radky; ++i)
+  {
+    delete[] matice[i];
+    matice[i] = nullptr;
+  }
+  delete[] matice;
+  matice = nullptr;
 }
